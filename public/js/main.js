f655505a16744ec861aa1a20b1e2d04f18e50cb2
ebhemmanuel <ebh.emmanuel@gmail.com>
2014-11-09 03:35:52 -0500

    got the fade to work on contact.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
 // $(".photoshop").text("Photoshop")
 // $(".illustrator").text("Illustrator")
 // $(".git").text("Git")
+$(function(){
+var link = $("#contact");
+var con  = $(".contact");
+	link.on('click',function(e){
+		e.preventDefault();
+			if (link.text() === ('Contact +')){
+				con.fadeIn(400);
+				link.text('Contact -');
+			}
+			else if
+				(link.text() == ('Contact -')){
+				con.fadeOut('400');
+				link.text('Contact +');
+			}
+
+		});
+});
